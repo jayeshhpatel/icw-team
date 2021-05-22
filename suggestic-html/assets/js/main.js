4bc2ea62e64027f2b193a899063aa6eabce2504e
jay.wpdemo <jay.wpdemo@gmail.com>
2021-05-22 17:49:33 +0530

    index page design complate

--- suggestic-html/assets/js/main.js
+++ suggestic-html/assets/js/main.js
@@ -1,11 +1,24 @@
-(function () {
 
-  var MenuIcon = document.querySelector('.menu-icon');
-  var Menu = document.querySelector('.menu-wrapper');
-  MenuIcon.addEventListener('click', function () {
-    Menu.classList.toggle('open');
-    MenuIcon.classList.toggle('open');
-    document.querySelector('body').classList.toggle('overflow-hidden');
-  });
+// Mobile MENU
+function toggleMenu() {
+  this.classList.toggle('open');
+  this.parentElement.classList.toggle("open");
+  document.querySelector('body').classList.toggle('overflow-hidden');
+}
+document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
 
-})();
+
+// Desktop MENU
+function toggleMenuDesktop() {
+  var w = document.documentElement.clientWidth;
+  // var h = document.documentElement.clientHeight;
+  if (w > 1199) {
+    document.querySelector("body").classList.remove('overflow-hidden');
+    document.querySelector(".header-left").classList.remove('open');
+    document.querySelector(".menu-icon").classList.remove('open');
+  }
+
+}
+window.addEventListener("resize", toggleMenuDesktop);
+
+new WOW().init();
