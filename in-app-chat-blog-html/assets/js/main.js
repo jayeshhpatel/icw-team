5400882d7d8b4ffa5931f8a8efcd0bb271e3db1e
jay.wpdemo <jay.wpdemo@gmail.com>
2021-05-24 19:54:32 +0530

    in app chat header design

---
+++ in-app-chat-blog-html/assets/js/main.js
@@ -0,0 +1,43 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+$(document).ready(function () {
+  $('[data-bs-toggle="tooltip"]').tooltip();
+
+
+  $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+    $('.menu-list,.bg-overly.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+  if ($('.main-header').length) {
+    $(window).scroll(function () {
+      if (jQuery(this).scrollTop() > 10) {
+        $(".main-header").addClass("fixed-header");
+      } else {
+        $(".main-header").removeClass("fixed-header");
+      }
+    });
+  }
+
+  /* scroll page to top */
+  if ($('.scroll-to-top').length) {
+    var scrollTop = $(".scroll-to-top");
+    $(window).scroll(function () {
+      var topPos = $(this).scrollTop();
+      if (topPos > 300) {
+        $(scrollTop).addClass("is-visible");
+      } else {
+        $(scrollTop).removeClass("is-visible");
+      }
+
+    });
+    //Click event to scroll to top
+    $(scrollTop).click(function () {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 300);
+      return false;
+    });
+  }
+});
