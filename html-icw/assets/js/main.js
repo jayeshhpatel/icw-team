6ef87f4fbace363ee3c48c728af1394500c86dde
jay.wpdemo <jay.wpdemo@gmail.com>
2021-05-26 17:59:43 +0530

    new page design html-icw

---
+++ html-icw/assets/js/main.js
@@ -0,0 +1,42 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+$(document).ready(function () {
+
+
+  $('.menu-btn').on('click', function (e) {
+    $('body').toggleClass('nav-menu-open');
+    e.preventDefault();
+  });
+
+  if ($('.main-header').length) {
+    $(window).scroll(function () {
+      if (jQuery(this).scrollTop() > 10) {
+        $(".main-header").addClass("fixed-header");
+      } else {
+        $(".main-header").removeClass("fixed-header");
+      }
+    });
+  }
+
+  /* scroll page to top */
+  if ($('.scroll-to-top').length) {
+    var scrollTop = $(".scroll-to-top");
+    $(window).scroll(function () {
+      var topPos = $(this).scrollTop();
+      if (topPos > 300) {
+        $(scrollTop).addClass("is-visible");
+      } else {
+        $(scrollTop).removeClass("is-visible");
+      }
+
+    });
+    //Click event to scroll to top
+    $(scrollTop).click(function () {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 300);
+      return false;
+    });
+  }
+});
