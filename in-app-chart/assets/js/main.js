d6843ad64508cd1714c06848571c1f39a663be2c
jay.wpdemo <jay.wpdemo@gmail.com>
2021-06-17 17:57:43 +0530

    chatbots ,chatops,peer-to-peer page design

--- in-app-chart/assets/js/main.js
+++ in-app-chart/assets/js/main.js
@@ -5,39 +5,20 @@
 
 $(document).ready(function () {
   $('.collapsible').collapsible();
+  $('.tooltipped').tooltip();
 
   $('.bar-icon,.bg-overly').on('click', function (e) {
     $('.menu-list,.bg-overly.bar-icon,body').toggleClass('is-visible');
     e.preventDefault();
   });
-  if ($('.main-header').length) {
-    $(window).scroll(function () {
-      if (jQuery(this).scrollTop() > 10) {
-        $(".main-header").addClass("fixed-header");
-      } else {
-        $(".main-header").removeClass("fixed-header");
-      }
-    });
-  }
 
-  /* scroll page to top */
-  if ($('.scroll-to-top').length) {
-    var scrollTop = $(".scroll-to-top");
-    $(window).scroll(function () {
-      var topPos = $(this).scrollTop();
-      if (topPos > 300) {
-        $(scrollTop).addClass("is-visible");
-      } else {
-        $(scrollTop).removeClass("is-visible");
-      }
+  $(window).scroll(function(){
+    if ($(window).scrollTop() >= 10) {
+        $('.main-header').addClass('fixed-header');
+    }
+    else {
+        $('.main-header').removeClass('fixed-header');
+    }
+});
 
-    });
-    //Click event to scroll to top
-    $(scrollTop).click(function () {
-      $('html, body').animate({
-        scrollTop: 0
-      }, 300);
-      return false;
-    });
-  }
 });
