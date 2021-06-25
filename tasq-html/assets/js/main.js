af88ae8a652b8e92c878b365a58302a7f2f9b766
jay.wpdemo <jay.wpdemo@gmail.com>
2021-06-25 09:33:59 +0530

    new pages design

--- tasq-html/assets/js/main.js
+++ tasq-html/assets/js/main.js
@@ -9,6 +9,7 @@ jQuery(function (o) {
 })
 $(document).ready(function () {
   $('[data-bs-toggle="tooltip"]').tooltip();
+  
   $('.toggle-sidebar,.bg-overly').on('click', function (e) {
     $('.navbar-collapse,.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
     e.preventDefault();
