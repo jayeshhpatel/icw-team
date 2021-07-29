1c85632b16d02cae214d267d1899ee0983f7ed32
jay.wpdemo <jay.wpdemo@gmail.com>
2021-07-29 19:42:27 +0530

    js file change

--- thrive-congess-html/assets/js/main.js
+++ thrive-congess-html/assets/js/main.js
@@ -26,16 +26,13 @@ $(document).ready(function () {
     $(".dropdown-menu-list").hide();
 
     // Shows dropdown-menu-list when it's parent is hovered.
-    $(".navbar-nav > li").mousemove(function () {
-      var ele = $(this);
-      $(ele).find("a + .dropdown-menu-list").not(':animated').slideDown(300);
+    $(".dropdown-menu-list").parent("li").hover(function () {
+      $(this).find(">.dropdown-menu-list").not(':animated').slideDown(300);
       $(this).toggleClass("active");
       $(".navbar").toggleClass("active");
-
-      sectionHeight = $(this).find('a + .dropdown-menu-list:not(:animated)').height();
-
-      $('head').append('<style>.main-header .navbar:after {height: ' + sectionHeight + 'px;}</style>');
-      console.log(sectionHeight);
+      sectionHeight = $(this).find(">.dropdown-menu-list").height();
+      $('<style>.main-header .navbar:after {height: ' + sectionHeight + 'px;}</style>').appendTo('head');
+      console.log(sectionHeight = $(this).find(">.dropdown-menu-list").height());
     });
 
     // Hides dropdown-menu-list when mouse leaves the zone.
