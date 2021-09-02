5db8cb5c83211aca514e04f0c71ae2e6618ea60c
jay.wpdemo <jay.wpdemo@gmail.com>
2021-09-02 11:56:48 +0530

    tooltip set

--- poisonpog-html/assets/js/main.js
+++ poisonpog-html/assets/js/main.js
@@ -2,7 +2,7 @@
 /* MAIN
 /*-----------------------------------------------------------------------------------*/
 $(document).ready(function () {
-  
+  $('[data-bs-toggle="tooltip"]').tooltip();
   $('.toggle-sidebar,.bg-overly').on('click', function (e) {
     $('.navbar-collapse,.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
     e.preventDefault();
