d610e161b69b0de18d694da357909de4151994d9
jay.wpdemo <jay.wpdemo@gmail.com>
2021-09-28 17:33:56 +0530

    responsive set

--- zib-html/assets/js/main.js
+++ zib-html/assets/js/main.js
@@ -22,28 +22,6 @@ $(document).ready(function () {
       }
     });
   }
-    
-
-  /* scroll page to top */
-  if ($('.scroll-to-top').length) {
-    var scrollTop = $(".scroll-to-top");
-    $(window).scroll(function () {
-      var topPos = $(this).scrollTop();
-      if (topPos > 300) {
-        $(scrollTop).addClass("is-visible");
-      } else {
-        $(scrollTop).removeClass("is-visible");
-      }
-
-    });
-    //Click event to scroll to top
-    $(scrollTop).click(function () {
-      $('html, body').animate({
-        scrollTop: 0
-      }, 300);
-      return false;
-    });
-  }
   
 });
 
