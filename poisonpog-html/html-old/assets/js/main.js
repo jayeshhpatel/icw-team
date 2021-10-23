7583c4e6fa014c698452ebbe9117dc19f803b29a
jay.wpdemo <jay.wpdemo@gmail.com>
2021-10-23 09:58:09 +0530

    new bg set

---
+++ poisonpog-html/html-old/assets/js/main.js
@@ -0,0 +1,41 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+$(document).ready(function () {
+  $('[data-bs-toggle="tooltip"]').tooltip();
+  $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+    $('.navbar-collapse,.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+    /* scroll page to top */
+    if ($('.scroll-to-top').length) {
+      var scrollTop = $(".scroll-to-top");
+      $(window).scroll(function () {
+        var topPos = $(this).scrollTop();
+        if (topPos > 600) {
+          $(scrollTop).addClass("is-visible");
+        } else {
+          $(scrollTop).removeClass("is-visible");
+        }
+  
+      });
+      //Click event to scroll to top
+      $(scrollTop).click(function () {
+        $('html, body').animate({
+          scrollTop: 0
+        }, 600);
+        return false;
+      });
+    }
+
+    $(window).scroll(function(){
+      if ($(window).scrollTop() >= 10) {
+          $('.main-header').addClass('fixed-header');
+      }
+      else {
+          $('.main-header').removeClass('fixed-header');
+      }
+    });
+
+});
