7404932f9b7eaa82b1133c5fb1bd840f02e962d5
jay.wpdemo <jay.wpdemo@gmail.com>
2021-10-25 20:00:05 +0530

    new pages design

--- poisonpog-html/assets/js/main.js
+++ poisonpog-html/assets/js/main.js
@@ -38,4 +38,11 @@ $(document).ready(function () {
       }
     });
 
+    $(".custom-html-dropdown .dropdown-menu li .dropdown-item").click(function(){
+      $('.custom-html-dropdown .dropdown-menu li .dropdown-item ').removeClass('active');
+      $(this).addClass('active');
+      $(this).parents(".custom-html-dropdown").find('.dropdown-input').html($(this).text() );
+      $(this).parents(".custom-html-dropdown").find('.dropdown-input').val($(this).data('value'));
+    });
+
 });
