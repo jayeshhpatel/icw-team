bc44bedf52ce8ffad97e4ed2a97f802f1f1a2646
jay.wpdemo <jay.wpdemo@gmail.com>
2021-12-03 11:33:03 +0530

    issue fixed

--- solis-html/assets/js/main.js
+++ solis-html/assets/js/main.js
@@ -3,13 +3,9 @@
 /*-----------------------------------------------------------------------------------*/
 $(document).ready(function () {
   $('[data-bs-toggle="tooltip"]').tooltip();
- 
+  
   $('.toggle-sidebar,.bg-overly').on('click', function (e) {
     $('.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
-    e.preventDefault();
-  });
-  $('.filter-btn').on('click', function (e) {
-    $('.filter-dropdown').toggleClass('is-open');
     e.preventDefault();
   });
 
@@ -22,57 +18,11 @@ $(document).ready(function () {
       }
     });
   }
-    
-
-  /* scroll page to top */
-  if ($('.scroll-to-top').length) {
-    var scrollTop = $(".scroll-to-top");
-    $(window).scroll(function () {
-      var topPos = $(this).scrollTop();
-      if (topPos > 300) {
-        $(scrollTop).addClass("is-visible");
-      } else {
-        $(scrollTop).removeClass("is-visible");
-      }
-
-    });
-    //Click event to scroll to top
-    $(scrollTop).click(function () {
-      $('html, body').animate({
-        scrollTop: 0
-      }, 300);
-      return false;
-    });
-  }
-  $('.testimonials-slider').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      responsive: [
-          {
-              breakpoint: 767,
-              settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1,
-                  arrows: false,
-                  dots: true
-              }
-          }
-      ]
-
-  });
-  $('.slick.marquee').slick({
-    speed: 5000,
-    autoplay: true,
-    autoplaySpeed: 0,
-    centerMode: true,
-    cssEase: 'linear',
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    infinite: true,
-    initialSlide: 1,
-    arrows: true,
-    buttons: false
-  });
 });
 
+ /* WOW Animation - Init */
+ try {
+  new WOW().init();
+} catch (e) {
+  //
+};
