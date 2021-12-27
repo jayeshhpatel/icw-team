cbecadb4650d3f9bbf0f9a4131f28023d7e2f470
jay.wpdemo <jay.wpdemo@gmail.com>
2021-12-27 19:02:51 +0530

    new html design start

--- solis-marketplace-html/assets/js/main.js
+++ solis-marketplace-html/assets/js/main.js
@@ -49,28 +49,64 @@ $(document).ready(function () {
     accessibility: false,
     arrows: false,
     responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-          breakpoint: 576,
+      {
+          breakpoint: 991,
           settings: {
-              slidesToShow: 1,
+              slidesToShow: 2,
               slidesToScroll: 1
           }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
       }
     ]
   });
-  $('.arrow-left').click(function(){
+  $('.threegrid-left').click(function(){
     $('.threegrid-slider').slick('slickPrev');
   })
-  
-  $('.arrow-right').click(function(){
+  $('.threegrid-right').click(function(){
     $('.threegrid-slider').slick('slickNext');
+  })
+
+  $('.fourgrid-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    accessibility: false,
+    arrows: false,
+    responsive: [
+      {
+          breakpoint: 991,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+          }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.fourgrid-left').click(function(){
+    $('.fourgrid-slider').slick('slickPrev');
+  })
+  $('.fourgrid-right').click(function(){
+    $('.fourgrid-slider').slick('slickNext');
   })
 });
 
