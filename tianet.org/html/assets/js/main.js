fa93dc606dc7a933086a5858698bcc99e45be799
jay.wpdemo <jay.wpdemo@gmail.com>
2022-01-10 18:51:17 +0530

    new page design

---
+++ tianet.org/html/assets/js/main.js
@@ -0,0 +1,39 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+jQuery(document).ready(function () {
+    jQuery('.owl-carousel.feature-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+    jQuery('.owl-carousel.category-slider').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            500:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+})
