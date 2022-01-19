61f8ea099ecfa8983564303b8bfc3cc16074dd0c
jay.wpdemo <jay.wpdemo@gmail.com>
2022-01-19 19:09:46 +0530

    new html design

--- in-app-chart-newsite/assets/js/main.js
+++ in-app-chart-newsite/assets/js/main.js
@@ -9,24 +9,15 @@ $(document).ready(function () {
     e.preventDefault();
   });
 
-
-  $(window).scroll(function(){
-    if ($(window).scrollTop() >= 10) {
-        $('.main-header').addClass('fixed-header');
-    }
-    else {
-        $('.main-header').removeClass('fixed-header');
-    }
-  });
-  $(".menu-item-has-children .arrow-down").click(function () {
-    if ($(this).closest(".menu-item-has-children").hasClass("open")) {
-        $(this).closest(".menu-item-has-children").removeClass("open");
-        $(this).closest(".menu-item-has-children").find(".sub-menu").slideUp();
-    } else {
-        $(".menu-item-has-children").removeClass("open");
-        $(".menu-item-has-children").find(".sub-menu").slideUp();
-        $(this).closest(".menu-item-has-children").addClass("open");
-        $(this).closest(".menu-item-has-children").find(".sub-menu").slideDown();
-    }
-  })
+  if($('.collapse-info-block').length) { 
+    $('.collapse-card .collaspe-header').on( 'click', function() { 
+        if($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $(this).parents('.collapse-card').find('.collapse-body').slideUp().removeClass('show');
+        } else {
+            $(this).addClass('active');
+            $(this).parents('.collapse-card').find('.collapse-body').slideDown().addClass('show');
+        }
+    });
+  }
 });
